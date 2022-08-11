bd3dda1d3c3290259db1610c0b73f39469246076
Ashif Mujtoba <nmujtoba@gmail.com>
2022-08-11 18:02:33 +0600

    done 1295

--- src/main/java/study/LinearSearch/EvenDigits.java
+++ src/main/java/study/LinearSearch/EvenDigits.java
@@ -1,2 +1,32 @@
-package study.LinearSearch;public class EvenDigits {
+package study.LinearSearch;
+/*
+ * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ */
+
+public class EvenDigits {
+    public static void main(String[] args) {
+        int ans = findNumbers(new int[]{12, 345, 2, 6, 7896});
+        System.out.println(ans);
+
+    }
+
+    static int findNumbers(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            int n = num;
+            int digitCount = 0;
+            while (n > 0) {
+                n = n / 10;
+                digitCount++;
+            }
+
+            if (digitCount % 2 == 0) {
+                result += 1;
+            }
+
+        }
+
+        return result;
+    }
 }
