0097e90373f7ace5bfbfcf464c1e02d437a9f606
Ashif Mujtoba <nmujtoba@gmail.com>
2022-08-18 00:15:27 +0600

    search in 2D array done

--- src/main/java/study/LinearSearch/SearchInSorted2DArray.java
+++ src/main/java/study/LinearSearch/SearchInSorted2DArray.java
@@ -12,7 +12,9 @@ public class SearchInSorted2DArray {
         };
         int target = 33;
 //        int[] ans = search(arr, target); // format of return value {row, col}
-        int[] ans = searchLinearBinary(arr, target); // format of return value {row, col}
+//        int[] ans = searchLinearBinary(arr, target); // format of return value {row, col}
+//        int[] ans = searchLinear(arr, target); // format of return value {row, col}
+        int[] ans = searchOnlyBinary(arr, target); // format of return value {row, col}
 
         System.out.println(Arrays.toString(ans));
     }
@@ -35,6 +37,49 @@ public class SearchInSorted2DArray {
             int result = binarySearch(arr[row], target);
             if (result != -1) {
                 return new int[]{row, result};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    //binary search O(log(NxM))
+    static int[] searchOnlyBinary(int[][] arr, int target) {
+        int start = 0;
+        int end = arr.length * arr[0].length - 1;
+        int columnLen = arr[0].length;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int value = arr[(mid / columnLen)][(mid % columnLen)];
+
+            if (target == value) {
+                return new int[]{(mid / columnLen), (mid % columnLen)};
+            } else if (target > value) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    //linear O(N) search
+    static int[] searchLinear(int[][] matrix, int target) {
+
+        int rowLen = matrix.length - 1;
+        int start = 0;
+        int end = rowLen;
+
+        while (start < rowLen && end >= 0) {
+            if (matrix[start][end] == target) {
+                return new int[]{start, end};
+            }
+            if (matrix[start][end] > target) {
+                end--;
+            } else {
+                start++;
             }
         }
         return new int[]{-1, -1};
